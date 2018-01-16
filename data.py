613c8a2bef49843875bd725f31229adfff0581c2
falcato <tomas.falcato.costa@ist.utl.pt>
2018-01-16 23:35:57 +0000

    register to db complete

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article 1',
+			'body': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
+			'author': 'Falcato',
+			'create_date': '2017-01-16'
+		},
+		{
+			'id': 2,
+			'title': 'Article 2',
+			'body': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
+			'author': 'Costa',
+			'create_date': '2017-01-16'
+		},
+		{
+			'id': 3,
+			'title': 'Article 3',
+			'body': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
+			'author': 'Falcato',
+			'create_date': '2017-01-16'
+		}
+	]
+	
+	return articles
